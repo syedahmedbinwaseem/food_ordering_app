ec01c306dd8aa8e13903ff2c93da1a42f70c84c2
syedahmedbinwaseem <57592567+syedahmedbinwaseem@users.noreply.github.com>
2021-02-25 23:57:58 +0500

    Fonts

---
+++ lib/utils/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+Color primaryGreen = Color(0xff00D589);
+Color darkGreen = Color(0xff245A3A);
+Color pink = Color(0xffF46060);
+Color blue = Color(0xff2C4068);
+Color lightBlue = Color(0xff9CAEC5);
+
+//Material Color for Primaryswatch
+Map<int, Color> primaryColor = {
+  50: Color.fromRGBO(0, 213, 137, .1),
+  100: Color.fromRGBO(0, 213, 137, .2),
+  200: Color.fromRGBO(0, 213, 137, .3),
+  300: Color.fromRGBO(0, 213, 137, .4),
+  400: Color.fromRGBO(0, 213, 137, .5),
+  500: Color.fromRGBO(0, 213, 137, .6),
+  600: Color.fromRGBO(0, 213, 137, .7),
+  700: Color.fromRGBO(0, 213, 137, .8),
+  800: Color.fromRGBO(0, 213, 137, .9),
+  900: Color.fromRGBO(0, 213, 137, .1),
+};
