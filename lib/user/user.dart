2edc8b5a731f0680e436944c41d6fced7ace6bd7
mehboobiqbal44 <73356175+mehboobiqbal44@users.noreply.github.com>
2021-03-01 22:00:35 +0500

    user

--- lib/user/user.dart
+++ lib/user/user.dart
@@ -1,1 +1,13 @@
-class User {}
+class User {
+  static final User _singleton = User._internal();
+  factory User() => _singleton;
+  User._internal();
+  static User get userData => _singleton;
+
+  String name;
+  String phone;
+  String city;
+  String gender;
+  String address;
+  String email;
+}
